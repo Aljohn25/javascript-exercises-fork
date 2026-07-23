b901f62e3a93fd3db3b41d5539f1874a66525df4
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-07-23 16:19:11 +0800

    Made a function that repeats strings and it has passed 4 tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+return str.repeat(count);
 
 };
 
+repeatString("hey", 3);
+repeatString("hello",10);
+repeatString("hi", 1);
+repeatString("bye", 0);
 // Do not edit below this line
 module.exports = repeatString;
