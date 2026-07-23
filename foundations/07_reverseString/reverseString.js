1bc9b8b98687cb74e2d6827cca8fdc08069da3bb
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-07-23 21:57:53 +0800

    created function to reverse str and passed all the tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(reverse) {
 
-};
+    let result = "";
+     for (let i = reverse.length - 1; i >= 0; i--) {
+    result += reverse[i];
+    }
+
+   return result;
+
+}
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
