219517b2b8c53cb0c9c877d2efb2fd772a5fc04e
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-07-24 16:17:00 +0800

    function 'start and end' range get sum and check for int, pass tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+if (start <= 0 || end <= 0 ) return "ERROR";
+if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+if (start > end) [start, end] = [end, start];
+
+let total = 0;
+for (let i = start; i <= end; i++) {
+    total += i;
+    }
+
+return total;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
