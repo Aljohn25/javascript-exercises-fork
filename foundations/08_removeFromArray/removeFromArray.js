0a99e2647d2a31eaf6001ec5df56c41dbe319706
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-07-24 15:45:05 +0800

    Function made with help of solution and then pass the test

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...index) {
+    
+    const newArray = [];
+
+  array.forEach((item) => {
+    
+    if (!index.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  
+  return newArray;
 };
+
+
+
+
+
+
+removeFromArray([1, 2, 3, 4], 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
