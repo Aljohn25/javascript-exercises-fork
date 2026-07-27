9b6209ceaa3531d49a7d4cf339f62245ca3f931b
Aljohn_Moorhead <282528950+Aljohn25@users.noreply.github.com>
2026-07-27 13:56:46 +0800

    add conversion formula and rounding off then pass the tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,12 +1,18 @@
 const convertToCelsius = function(tempf) {
-  conc = ((tempf - 32) * 5/9);
-  return conc;
+  let conc = ((tempf - 32) * 5/9);
+  let rounded = Number((conc).toFixed(1));
+  return rounded;
 };
 
 convertToCelsius(32);
 
-const convertToFahrenheit = function(celfa) {
+const convertToFahrenheit = function(tempc) {
+  let conf = ((tempc * (9/5)) + 32);
+  let rounded2 = Number((conf).toFixed(1));
+  return rounded2;
 };
+
+convertToFahrenheit();
 
 // Do not edit below this line
 module.exports = {
